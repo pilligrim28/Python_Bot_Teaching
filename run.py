c366557e80b58b2115fd895da7600566b0f4f8fc
pilligrim28 <balu-133@ya.ru>
2024-08-22 19:59:18 +0300

    Сделал id

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ dp = Dispatcher()
 
 @dp.message(CommandStart()) #ждет комнады старт handler
 async def cmd_start(message: Message):
-    await message.answer('Привет, Кракозябра')
+    await message.answer(f'Привет, \nтвой ID: {message.from_user.id}\nИмя: {message.from_user.full_name}')
 
 @dp.message(Command('help')) #ждет комнады help handler
 async def get_help(message: Message):
@@ -21,6 +21,14 @@ async def get_help(message: Message):
 @dp.message(F.text == 'Как дела?') #Вопрос ответ
 async def how_are_you(message: Message):
     await message.answer('Ok!')
+
+@dp.message(F.photo)
+async def get_photo(message: Message):
+    await message.answer(f'ID фото: {message.photo[-1].file_id}')
+
+@dp.message(Command('get_photo'))
+async def get_photo(message: Message):
+    await message.answer_photo(photo='AgACAgIAAxkBAAMYZsdJ1QvTu_uavkLRFsfFKuG4rkUAAojhMRtHPTlK_Cvcw8CkTSIBAAMCAANtAAM1BA', caption = 'Это запрос Json')
 
 
 async def main():
