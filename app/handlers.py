52b2a76952e7896160c07d9f6762624b62e301dd
pilligrim28 <balu-133@ya.ru>
2024-08-23 10:13:18 +0300

    Создание хэндлеров в отдельном файле

---
+++ app/handlers.py
@@ -0,0 +1,27 @@
+from aiogram import F, Router
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+router=Router()
+
+
+@router.message(CommandStart()) #ждет комнады старт handler
+async def cmd_start(message: Message):
+    await message.reply(f'Привет, \nтвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}')
+
+@router.message(Command('help')) #ждет комнады help handler
+async def get_help(message: Message):
+    await message.answer('Это команда /help')
+
+@router.message(F.text == 'Как дела?') #Вопрос-ответ
+async def how_are_you(message: Message):
+    await message.answer('Ok!')
+
+@router.message(F.photo)
+async def get_photo(message: Message):
+    await message.answer(f'ID фото: {message.photo[-1].file_id}')
+
+@router.message(Command('get_photo'))
+async def get_photo(message: Message):
+    await message.answer_photo(photo='AgACAgIAAxkBAAMYZsdJ1QvTu_uavkLRFsfFKuG4rkUAAojhMRtHPTlK_Cvcw8CkTSIBAAMCAANtAAM1BA', caption = 'Это запрос Json')
+
--- run.py
+++ run.py
@@ -1,37 +1,16 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, F
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher
+
 
 from config import TOKEN #Импортирование токена с файла
+from app.handlers import router
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-
-@dp.message(CommandStart()) #ждет комнады старт handler
-async def cmd_start(message: Message):
-    await message.reply(f'Привет, \nтвой ID: {message.from_user.id}\nИмя: {message.from_user.first_name}')
-
-@dp.message(Command('help')) #ждет комнады help handler
-async def get_help(message: Message):
-    await message.answer('Это команда /help')
-
-@dp.message(F.text == 'Как дела?') #Вопрос-ответ
-async def how_are_you(message: Message):
-    await message.answer('Ok!')
-
-@dp.message(F.photo)
-async def get_photo(message: Message):
-    await message.answer(f'ID фото: {message.photo[-1].file_id}')
-
-@dp.message(Command('get_photo'))
-async def get_photo(message: Message):
-    await message.answer_photo(photo='AgACAgIAAxkBAAMYZsdJ1QvTu_uavkLRFsfFKuG4rkUAAojhMRtHPTlK_Cvcw8CkTSIBAAMCAANtAAM1BA', caption = 'Это запрос Json')
-
-
 async def main():
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
